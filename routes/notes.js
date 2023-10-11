c6ed1f140063145102acc8953aa0fa10b9023575
afong <adam.y.w.fong@gmail.com>
2023-10-11 09:55:11 -0700

    removed uuid dependency

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,6 @@ const {
     readAndAppend,
     writeToFile,
   } = require('../helpers/fsUtils');
-const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
@@ -26,7 +25,7 @@ notes.delete('/:note_id', (req, res) => {
     });
 });
 
-// POST Route for a new UX/UI note
+// POST Route for a new note
 notes.post('/', (req, res) => {
   console.log(req.body);
 
@@ -36,7 +35,7 @@ notes.post('/', (req, res) => {
     const newNote = {
       title,
       text,
-      id: uuidv4()
+      id: Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
     };
     readAndAppend(newNote, './db/db.json');
   } else {
